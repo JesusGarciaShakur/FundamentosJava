018438c9fb9c8e48884cff1b442d351837b9241d
JesusGarciaShakur <jesusrguez970@gmail.com>
2025-01-18 19:47:22 -0600

    Refactor Code Switch

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,45 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int resultado = suma(12,25);
-        System.out.println(resultado);
-
+        Scanner scanner = new Scanner(System.in);
+        cerrar : while (true){
+        mostrarOpciones();
+            int numero = scanner.nextInt();
+            switch (numero) {
+                case 1:
+                    int resultado = calculadora(scanner);
+                    System.out.println("Tu resultado es: " + resultado);
+                    break;
+                case 2:
+                    hackearNasa();
+                    break;
+                case 3:
+                    System.out.println("saliendo");
+                    break cerrar;
+                default:
+                    System.out.println("Seleccione una opcion correcta");
+            }
+        }
     }
-    //Nombre es un parametro y el valor que le pasasmos es un argumento
-    static int suma(int a, int b){
-        return a+b;
+
+    private static int calculadora(Scanner scanner) {
+        System.out.println("Ingresa el primer numero");
+        int numero1 = scanner.nextInt();
+        System.out.println("Ingresa el segundo numero");
+        int numero2 = scanner.nextInt();
+        int solucion = numero1 + numero2;
+        return solucion;
+    }
+
+    private static void hackearNasa() {
+        System.out.println("Estas hackeando la nasa");
+        System.out.println("Eres un pro");
+    }
+
+    private static void mostrarOpciones(){
+        System.out.println("Ingrese una opcion");
+        System.out.println("1.Calculadora");
+        System.out.println("2.Hackear la NASA");
+        System.out.println("3.Salir");
     }
 }
